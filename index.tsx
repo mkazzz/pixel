a9f449d76bf931120ac18d002299cc6534751451
mkazzz <110970481+mkazzz@users.noreply.github.com>
2025-06-16 07:36:56 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from 'pixel/App';
 // No global CSS import needed as Tailwind is via CDN
 
 const rootElement = document.getElementById('root');
